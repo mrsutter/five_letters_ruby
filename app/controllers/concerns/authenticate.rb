a53a1380f8565eabb1a4ef63da9b624819c1fab8
Paul Yakshankin <yakspavel@gmail.com>
2023-05-12 07:29:47 +0530

    refactored authenticate

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -9,21 +9,21 @@ module Authenticate
     before_action :authenticate_user!
   end
 
+  attr_reader :token, :current_user
+
   def authenticate_user!
-    @user_id, jti = JwtToken.decode(
+    user_id, jti = JwtToken.decode(
       value: auth_token, public_key: Tokens::AccessToken::PUB_KEY
     )
-    raise NotAuthenticated if @user_id.blank?
 
-    token = Tokens::AccessToken.find_by(jti: jti, user_id: @user_id)
+    @current_user = User.find_by(id: user_id)
+    raise NotAuthenticated if user_id.blank?
+
+    @token = Tokens::AccessToken.find_by(jti: jti, user: current_user)
     raise NotAuthenticated if token.blank?
   end
 
   def auth_token
     request.headers['Authorization'].to_s.sub('Bearer ', '')
-  end
-
-  def current_user
-    @current_user ||= User.find(@user_id)
   end
 end
